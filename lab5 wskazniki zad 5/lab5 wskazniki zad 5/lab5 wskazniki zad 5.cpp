83ffd41f46a0ccde80e67e48145adecfcd022a6f
Rafał <r.chorubski@gmail.com>
2019-02-11 11:45:13 +0100

    lab5 wskazniki zad 5

---
+++ lab5 wskazniki zad 5/lab5 wskazniki zad 5/lab5 wskazniki zad 5.cpp
@@ -0,0 +1,44 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <iomanip>
+#include "tchar.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string>
+
+
+using namespace std;
+
+
+
+int zamiana(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+	return 0;
+}
+
+
+int main() {
+
+	int  x5, y5, z5;
+
+
+	cout << "5. Zamiana: " << endl;
+	x5 = 5;
+	y5 = 7;
+	cout << " a = " << x5 << "  b = " << y5;
+	z5 = zamiana(&x5, &y5);
+
+	cout << "\nPo zamianie: a=" << x5 << "  b = " << y5;
+	getchar();
+	return 0;
+
+}
